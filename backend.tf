ac16355acdfef4a15deef41c62c3ceb666c91a74
Karma Lama <karmalama@karmas-air-2.lan>
2020-02-20 20:11:40 -0500

    change

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket = "terraform-arun"
     key    = "terraform/dev/helloapp"
     region = "us-east-1"
+    shared_credentials_file = "/home/intelycorellc/.aws/credentials"
     profile = "arun"
   }
 }
